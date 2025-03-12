562a5adbc5b4b0e79a519462b95a90239f31baee
notDuyLam <22120181@student.hcmus.edu.vn>
2025-03-12 09:53:20 +0700

    fix database for deploy

--- config/database.js
+++ config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("postgres", "postgres", "123123", {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    logging: false
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
